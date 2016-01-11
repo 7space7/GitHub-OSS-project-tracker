c77b96c102e8b0be0450faa893339933ea7b1cb8
Viktor Vaidner <space10@mail.ru>
2016-01-12 00:20:39 +0200

    add roboelectric and jacoco

--- app/src/test/java/com/ua/viktor/social/ExampleUnitTest.java
+++ app/src/test/java/com/ua/viktor/social/ExampleUnitTest.java
@@ -1,12 +1,17 @@
 package com.ua.viktor.social;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
